e19f03b1bc68833ed23ada6bdebf44fd7e5d7a60
ayush2116 <ayushforever21@gmail.com>
2022-10-08 14:10:49 +0530

    first-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Resturant from './component/basics/Resturant'
+const App = () => {
+  return (
+    <div>
+      <Resturant />
+    </div>
+  )
+}
+
+export default App
